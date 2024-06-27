313f6d4e7d43e2012bc9a7ad1f1c66c4a99fd582
Diego Lavayen Alarcon <diego.lavayen.alarcon@gmail.com>
2024-06-27 18:26:09 +0100

    Add not found component for 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function RootNotFound() {
+    return (
+        <main className="full-page">
+            <div className="grid grid-cols-2 divide-x text-2xl align-baseline">
+                <div className="flex flex-col text-right px-2">404</div>
+                <h1 className='px-2'>Not Found</h1>
+            </div>
+        </main>
+    )
+}
